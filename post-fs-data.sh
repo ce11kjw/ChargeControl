4644c82952778086d3b9f19fcdef68d999aea26e
ce11kjw <akl1231122@gmail.com>
2026-04-06 10:12:07 +0800

    Update post-fs-data.sh with new initialization script

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -7,3 +7,9 @@ LOG="$MODDIR/module.log"
 log() { echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*" >> "$LOG"; }
 
 log "post-fs-data.sh started"
+
+# Ensure the charge_control binary is executable at boot
+if [ -f "$MODDIR/charge_control" ]; then
+    chmod 777 "$MODDIR/charge_control"
+    log "INFO: chmod 777 applied to charge_control"
+fi
